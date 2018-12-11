0bf5590748002095c99b874a44f050362bd36525
fursa.web <fursa.web@gmail.com>
2018-12-11 22:01:16 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
+import Header from './Header.js';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Decoblog</h1>
-        </header>
+      <div>
+        <Header/>
       </div>
     );
   }
